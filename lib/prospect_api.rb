3ea70786ab6e431fc64e0641abde83ad68af358d
Vais Mahendran <vais@careerjsm.com>
2020-02-19 11:09:02 -0500

    Cleanup and minor refactor
    Refactoring to support multiple leagues instead of hardcoded league

--- lib/prospect_api.rb
+++ lib/prospect_api.rb
@@ -8,60 +8,60 @@ module Scraper
 
     base_uri 'https://lscluster.hockeytech.com/feed/'
 
+    def initialize
+      @default_options = {
+        feed: 'modulekit',
+        key: '2976319eb44abe94',
+        client_code: 'ohl',
+        season_id: '68',
+        fmt: 'json'
+      }
+    end
+
     def schedule
-      response = request("", view: 'schedule')
+      response = request('', view: 'schedule')
       response.dig('SiteKit', 'Schedule')
     end
 
     def teams
-      response = request("", view: 'teamsbyseason')
+      response = request('', view: 'teamsbyseason')
       response.dig('SiteKit', 'Teamsbyseason')
     end
 
     def player(id)
-      response1 = request("", player_id: id, view: 'player', category: 'profile')
-      response2 = request("", player_id: id, view: 'player', category: 'seasonstats')
+      response1 = request('', player_id: id, view: 'player', category: 'profile')
+      response2 = request('', player_id: id, view: 'player', category: 'seasonstats')
 
-      { 
-        profile: response1.dig('SiteKit', 'Player'), 
-        stats:   response2.dig('SiteKit', 'Player')
+      {
+        profile: response1.dig('SiteKit', 'Player'),
+        stats: response2.dig('SiteKit', 'Player')
       }
     end
 
     def skaters
-      response = request("", view: 'statviewtype', type: 'topscorers', first: 0, limit: 1000, sort: 'active')
-      response.dig('SiteKit', 'Statviewtype').filter { |player| player["position"] != 'G' }
+      response = request('', view: 'statviewtype', type: 'topscorers', first: 0, limit: 1000, sort: 'active')
+      response.dig('SiteKit', 'Statviewtype').reject! { |player| player["position"] == 'G' }
     end
 
     def goalies
-      response = request("", view: 'statviewtype', type: 'topgoalies',  first: 0, limit: 1000, sort: 'active')
+      response = request('', view: 'statviewtype', type: 'topgoalies', first: 0, limit: 1000, sort: 'active')
       response.dig('SiteKit', 'Statviewtype')
     end
 
     private
 
-    def request(path = "", options = {})
+    def request(path = '', options = {})
       response = self.class.get(path, params(options))
 
-      if (response.code == 200)
-        JSON.parse(response.body, )
+      if response.code == 200
+        JSON.parse(response.body)
       else
         {}
       end
     end
 
-    def default_options
-      { 
-        feed: 'modulekit',
-        key: '2976319eb44abe94',
-        client_code: 'ohl',
-        season_id: '68',
-        fmt: 'json',
-      }
-    end
-
     def params(options)
-      { query: default_options.merge(options) }
+      { query: @default_options.merge(options) }
     end
   end
 end
